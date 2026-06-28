3bb864dbde84b503e0e42cd12f3f743cf8cb6018
PedroHBertoni <phbertoni290307ph@gmail.com>
2026-06-28 15:44:32 -0300

    ta quase pronto

--- computadorias.c
+++ computadorias.c
@@ -5,22 +5,49 @@
 #include <math.h>
 
 typedef unsigned long long formatoBinario;
-formatoBinario memoria[1000];
+formatoBinario memoria[1000]; 
 FILE *arquivo;
 char linha[100];
 
+formatoBinario MASCARA_Endereco  = 0b00000000111111111111;
+formatoBinario MASCARA_Opcode    = 0b11111111000000000000;
+formatoBinario MASCARA_40Bits    = 0b1111111111111111111111111111111111111111;
+formatoBinario MASCARA_20Bits    = 0b0000000000000000000011111111111111111111;
+
+formatoBinario MASCARA_00_07Bits = 0b0000000011111111111111111111111111111111;
+formatoBinario MASCARA_08_19Bits = 0b1111111100000000000011111111111111111111;
+formatoBinario MASCARA_20_27Bits = 0b1111111111111111111100000000111111111111;
+formatoBinario MASCARA_28_39Bits = 0b1111111111111111111111111111000000000000;
+
 struct CPU {
-    unsigned short PC;  // Program Counter
-    unsigned short IR;  // Instruction Register
-    unsigned short MAR; // Memory Address Register
-    long int MBR;       // Memory Buffer Register
-    long int AC;        // Acumulador
-    long int M;         // Multiplicador
-    long int R;         // Resto da divisão
-    bool C;             // Carry (0 ou 1)
-    bool N;             // 0 (positivo) ou 1 (negativo)
-    bool Z;             // 0 (diferente de zero) ou 1 (zero)
-};
+    unsigned short PC;  // Program Counter                      12 bits
+    
+    unsigned short IR;  // Instruction Register                 8  bits
+    unsigned short MAR; // Memory Address Register              12 bits
+    unsigned short IBR; // Instruction Buffer Register          20 bits
+    
+    long int MBR;       // Memory Buffer Register               40 bits
+    long int AC;        // Acumulador                           40 bits
+    long int MQ;        // Multiplicador Quociente              40 bits
+
+    bool LorR;          // 0 instrucao de IR, 1 instr de IBR    1  bit FLAG
+    bool leituraCompleta// 0 le so a direita, 1 le esq+dir      1  bit FLAG
+} cpu;
+
+void criar_CPU() {
+    cpu.AC  = 0b0 << 40ULL;
+    cpu.MQ  = 0b0 << 40ULL;
+    cpu.MBR = 0b0 << 40ULL;
+    cpu.PC  = 0b0 << 12ULL;
+
+    cpu.MAR = 0b0 << 12ULL;
+    cpu.IR  = 0b0 <<  8ULL;
+    cpu.IBR = 0b0 << 20ULL;
+
+    cpu.LorR = 0;
+    cpu.leituraCompleta = 1;
+    return;
+}
 
 formatoBinario codificador(char linha[100], int informacao){
     /*Responsável por codificar *linha* para o formato correto a ser armazenado na memória*/
@@ -162,7 +189,7 @@ int gerenciador_memoria(){
     int fim_informacao = 0; // flag que indica se o endereço já foi extraido
     int informacao = 0;     // pode ser um endereço ou um dado
     int numero;             // buffer que armazena o caractere convertido para inteiro
-    int teste = 0;
+
 
     if (arquivo == NULL) {
         printf("ERRO : Arquivo invalido/nao encontrado.\n");
@@ -197,7 +224,6 @@ int gerenciador_memoria(){
             if(linha[0] == ' ' || linha[0] == '-'){
                 if (posicao_dados == 100) {printf("ERRO LOGICO:\n - Este computador NÃO suporta mais de 100 dados."); }
                 memoria[posicao_dados] = palavra;
-                teste = posicao_dados;
                 posicao_dados++;
 
             }else{
@@ -217,147 +243,148 @@ int gerenciador_memoria(){
     }
 
     fclose(arquivo);
+    memoria[posicao_instrucao] = 0b0 << 39;
     return 0;
 }
 
-void executor(formatoBinario codigo, struct CPU cpu) {
-    formatoBinario opcode = codigo >> 12ULL;
-    formatoBinario mascara = (1ULL << 12)-1;
-    
-    if(codigo == 0b00001010){       // LOAD MQ
-        cpu.AC = cpu.M;
+
+
+void ciclo_busca() {
+    if (cpu.leituraCompleta) {
+        if (cpu.LorR) {                             // Falta executar instrucao da esquerda
+            cpu.IR  = cpu.IBR >> 12ULL;
+            cpu.MAR = cpu.IBR & MASCARA_Endereco;
+
+            cpu.PC++;
+            cpu.LorR = 0;
+        } else {                                    // Leitura de Novas Intrucoes
+            cpu.MAR = cpu.PC;
+            cpu.MBR = memoria[cpu.MAR];
+
+            cpu.IR  = (cpu.MBR >> 32ULL);
+            cpu.MAR = (cpu.MBR >> 20ULL) & MASCARA_Endereco;
+            cpu.IBR =  cpu.MBR & MASCARA_20Bits;
+            cpu.LorR = 1;
+        }
+    } else {
+        cpu.MAR = cpu.PC;
+        cpu.MBR = memoria[cpu.MAR];
+
+        cpu.IR  = cpu.MBR >> 12ULL;
+        cpu.MAR = cpu.MBR & MASCARA_Endereco;
+
+        cpu.PC++;
+        cpu.LorR = 0;
+        cpu.leituraCompleta = 1;
+    }
+}
+
+void ciclo_execucao() {
+    unsigned short opcode = cpu.IR;
+    int endereco = cpu.MAR;
+
+    if(opcode == 0b00001010){       // LOAD MQ
+        cpu.AC = cpu.MQ;
 
     }else if(opcode == 0b00001001){ // LOAD MQ.M(X)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.M = memoria[informacao];
+        cpu.MQ = memoria[endereco];
 
     }else if(opcode == 0b00100001){ // STOR M(X)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        memoria[informacao] = cpu.AC;
+        memoria[endereco] = cpu.AC;
 
     }else if(opcode == 0b00000001){ // LOAD M(X)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC = memoria[informacao];
+        cpu.AC = memoria[endereco];
 
     }else if(opcode == 0b00000010){ // LOAD-M(X)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC = memoria[informacao] * -1;
+        cpu.AC = memoria[endereco] * -1;
 
     }else if(opcode == 0b00000011){ // LOAD |M(X)|
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC = abs(memoria[informacao]);
+        cpu.AC = abs(memoria[endereco]);
 
     }else if(opcode == 0b00000100){ // LOAD-|M(X)|
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC = abs(memoria[informacao]) * -1;
+        cpu.AC = abs(memoria[endereco]) * -1;
         
-    }else if(opcode == 0b00001101){ // JUMP M(X,0:19)   TODOS OS JUMPS TENHO Q REVER AINDA
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.PC = informacao;
+    }else if(opcode == 0b00001101){ // JUMP M(X,0:19)
+        cpu.PC = endereco;
+        cpu.LorR = 0;
 
     }else if(opcode == 0b00001110){ // JUMP M(X,20:39)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.PC = informacao;
+        cpu.PC = endereco;
+        cpu.leituraCompleta = 0;
 
     }else if(opcode == 0b00001111){ // JUMP+M(X,0:19)
         if (cpu.AC >= 0) {
-            formatoBinario mascara = (1ULL << 12)-1;
-            int informacao = codigo & mascara;
-            cpu.PC = informacao;
+            cpu.PC = endereco;
+            cpu.LorR = 0;
         }
 
     }else if(opcode == 0b00010000){ // JUMP+M(X,20:39)
         if (cpu.AC >= 0) {
-            formatoBinario mascara = (1ULL << 12)-1;
-            int informacao = codigo & mascara;
-            cpu.PC = informacao;
+            cpu.PC = endereco;
+            cpu.leituraCompleta = 0;
         }
 
     }else if(opcode == 0b00000101){ // ADD M(X)
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC += memoria[informacao];
+        cpu.AC += memoria[endereco];
 
     }else if(opcode == 0b00000111){ // ADD |M(X)|
-        formatoBinario mascara = (1ULL << 12)-1;
-        int informacao = codigo & mascara;
-        cpu.AC += abs(memoria[informacao]);
-
+        cpu.AC += abs(memoria[endereco]);
 
     }else if(opcode == 0b00000110){ // SUB M(X)
-        
+        cpu.AC -= memoria[endereco];
+
     }else if(opcode == 0b00001000){ // SUB |M(X)|
-        
+        cpu.AC -= abs(memoria[endereco]);
+
     }else if(opcode == 0b00001011){ // MUL M(X)
-        
+        cpu.MQ = (cpu.MQ * memoria[endereco]) & MASCARA_40Bits;  // Nao sei se cpu.MQ * memoria[endereco] cabe na memoria
+        cpu.AC = (cpu.MQ * memoria[endereco]) >> 40ULL;
+
     }else if(opcode == 0b00001100){ // DIV M(X)
-        
+        cpu.AC /= memoria[endereco];
+        cpu.MQ  %= memoria[endereco];
+
     }else if(opcode == 0b00010100){ // LSH
+        cpu.AC = cpu.AC << 1;
         
     }else if(opcode == 0b00010101){ // RSH
+        cpu.AC = cpu.AC >> 1;
         
     }else if(opcode == 0b00010010){ // STOR M(X,8:19)
+        int informacao = (cpu.AC & MASCARA_Endereco) << 20ULL;
+        memoria[endereco] = memoria[endereco] & MASCARA_08_19Bits;
+        memoria[endereco] = memoria[endereco] | informacao;
         
     }else if(opcode == 0b00010011){ // STOR M(X,28:39)
-        
-    }else{
-        // if(linha[0] == '-'){
-        //     numero_negativo = informacao * -1;
-        //     mascara = (1ULL << 40) - 1;
-        //     palavra = numero_negativo & mascara;
-        // }else{
-            formatoBinario palavra = codigo;
-        // }
-    }
+        int informacao = cpu.AC & MASCARA_Endereco;
+        memoria[endereco] = memoria[endereco] & MASCARA_28_39Bits;
+        memoria[endereco] = memoria[endereco] | informacao;
+
+    }else if(opcode == 0b00000000){
+
+        printf("ERRO : Opcode nao identificado.");
+    } 
+    return;
 }
 
-/*
-void load_MQ () {
-    AC = M; }
-void load_MQ_Mx () {
-    M = Memoria[0]; }
-void stor_Mx () {
-    Memoria[0] = M; }
-void load_Mx () {
-    AC = Memoria[0]; }
-void load_menos_Mx () {
-    AC -= Memoria[0] }
-void load_abs_Mx () {
-    AC = 0 + Memoria[0]; }
-void load_abs_menos_Mx () {
-    AC -= 0 + Memoria[0]; }
-
-void jump_Mx_esq () {} 
-void jump_Mx_dir () {}
-
-void jump_cond_Mx_esq () {}
-void jump_cond_Mx_dir () {}
-
-void add_Mx () {}
-void add_abs_Mx () {}
-void sub_Mx () {}
-void sub_abs_Mx () {}
-void mul_Mx () {}
-void div_Mx () {}
-void lsh () {}
-void rsh () {}
-
-void stor_Mx_esq () {}
-void stor_Mx_dir () {}
-*/
+void ciclo_instrucao(formatoBinario codigo){
+    ciclo_busca();
+    ciclo_execucao();
+}
 
 
+void cpu_print(int i){
+    printf(" - %d esimo(a) execucao.", i);
+    printf("\n - MBR : %lu", cpu.MBR);
+    printf("\n - AC  : %lu  |  MQ  : %lu", cpu.AC, cpu.MQ);
+    printf("\n - IBR : %hu", cpu.IBR);
+    printf("\n - IR  : %hu  |  MAR : %hu", cpu.IR, cpu.MAR);
+    printf("\n - PC  : %hu", cpu.PC);
+}
 
 void main() {
+    criar_CPU();
+    gerenciador_memoria();
 
-gerenciador_memoria();
-
+    
 }
